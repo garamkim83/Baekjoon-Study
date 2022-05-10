6be70d7861e06c3ce9099e2ed50c3c16388759d7
garamkim83 <garamk83@gmail.com>
2022-05-10 17:50:05 +0900

    Create baekjoon13335.cpp
    13335 아직 하는 중...

---
+++ baekjoon13335/baekjoon13335.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//13335 ÇÏ´Â Áß...
+int main(void) {
+	int n, w, l, left, t = 0;
+	int weight[1000], pos[1000];
+
+	cin >> n >> w >> l;
+	left = n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> weight[i];
+		pos[i] = w + 1;
+	}
+	int num = 0;
+	while (left > 0) {
+		t++;
+	}
+	return 0;
+}
