391220dcca95e6e60d6d343daf9a1170ae768803
garamkim83 <garamk83@gmail.com>
2022-05-20 23:36:38 +0900

    Update baekjoon2439.cpp

--- baekjoon2439/baekjoon2439.cpp
+++ baekjoon2439/baekjoon2439.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
-int main(void) {
+int main() {
 	int n;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		for (int j = n; j < i - j; j++)
-			printf(" ");
-		for (int j = ; j < i; j++)
-			printf("*");
-		printf("\n");
+		for (int j = 0; j < n - i; j++)
+			cout << " ";
+		for (int j = 0; j < i; j++)
+			cout << "*";
+		cout << "\n";
 	}
 
 	return 0;
