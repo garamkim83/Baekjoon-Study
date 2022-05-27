a55995a97d7ccf668519bacfaef419f11e40a55d
garamkim83 <garamk83@gmail.com>
2022-05-28 00:04:28 +0900

    Create baekjoon2444.cpp

---
+++ baekjoon2444/baekjoon2444.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < n - i; j++)
+			cout << " ";
+		for (int j = 1; j <= 2 * i - 1; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < n - i; j++)
+			cout << " ";
+		for (int j = 0; j < 2 * i - 1; j++)
+			cout << "*";
+		cout << "\n";
+	}
+
+	return 0;
+}
