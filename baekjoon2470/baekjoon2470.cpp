b13ba0b0735eb296f39ae36e27c4c943571b9d09
garamkim83 <garamk83@gmail.com>
2022-07-02 00:38:07 +0900

    2470 두 용액

--- baekjoon2470/baekjoon2470.cpp
+++ baekjoon2470/baekjoon2470.cpp
@@ -4,19 +4,23 @@ using namespace std;
 
 int val[100000] = { 0, };
 int main() {
-	int n, sp = 2000000001, ans[2];
+	int n, sp = 2000000000, ans[2];
 	cin >> n;
 	for (int i = 0; i < n; i++) cin >> val[i];
 	sort(val, val + n);
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = 0; j < n; j++) {
-			int sum = abs(val[i] + val[j]);
-			if (sum < sp) {
-				sp = sum;
-				ans[0] = val[i];
-				ans[1] = val[j];
-			}
+	int f = 0, b = n - 1;
+	while (f < b)
+	{
+		int sum = val[f] + val[b];
+		if (sp > abs(sum)) {
+			sp = abs(sum);
+			ans[0] = val[f];
+			ans[1] = val[b];
+
+			if (sum == 0) break;
 		}
+		if (sum < 0) f++;
+		else b--;
 	}
 	cout << ans[0] << " " << ans[1];
 
