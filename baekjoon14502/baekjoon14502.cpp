1f55595dd822edf25b476638e7d08a0d8d00725e
garamkim83 <garamk83@gmail.com>
2022-07-04 22:07:59 +0900

    Create baekjoon14502.cpp

---
+++ baekjoon14502/baekjoon14502.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int n, m;
+int map[9][9];
+
+
+void buildWall() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) cin >> map[i][j];
+	}
+
+	return 0;
+}
