31be70bd5a038323fe32abf3ce7d2750e913f682
garamkim83 <garamk83@gmail.com>
2022-09-25 18:18:03 +0900

    9012 괄호

--- baekjoon9012/baekjoon9012.cpp
+++ baekjoon9012/baekjoon9012.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
+#include<string>
+using namespace std;
 
-int main(){
-    int t;
-    int parent[50][50];
-    int opPar=0, clPar=0;
-    std::cin >> t;
+int main() {
+	int n;
+	string s;
 
-    for(int i = 0; i < t; i++){
-        
-    }
-    return 0;
+	cin >> n;
+	getchar();
+	for (int i = 0; i < n; i++) {
+		int l = 0, r = 0;
+		getline(cin, s);
+		for (int j = 0; j < s.size(); j++) {
+			if (l == 0 && s[j] == ')') {
+				l--;
+				break;
+			}
+			if (s[j] == '(') l++;
+			else if (s[j] == ')') l--;
+		}
+		if (l == 0) cout << "YES\n";
+		else if (l != 0)cout << "NO\n";
+	}
+	return 0;
 }
